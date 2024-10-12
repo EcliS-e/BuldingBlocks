338070ffd4fbafec786e76c6599cf4c8ef024fb5
EcliS-e <marianita345@icloud.com>
2024-10-12 12:39:55 -0400

    Complete

---
+++ BuildingBlocks_Espinal/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Variables
+            string playerName = "adventurer";
+            string userChoice = "NONE";
+
+            //Program Start
+            Console.Write("Welcome to the jungle\nWe got fun and games\nWe got everything you want\nHoney, we know the names\n\n");
+            Console.WriteLine("Wait... I don't actually know your name.");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("[Please enter your name, then press 'ENTER' on the keyboard]");
+            //Grab some user input
+            playerName = Console.ReadLine();
+            Console.WriteLine("\nWelcome to the jungle " + playerName + "!");
---
+++ BuildingBlocks_Espinal/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string square = "Square";
+            string circle = "circle";
+
+            Console.WriteLine(circle, square);
+            PrintNick();
+
+            Console.WriteLine(TwoNums(3, 4));
+
+
+
+
+            void PrintNick()
+            {
+                Console.WriteLine("Nick Buonorata");
+
+            }
+
+            int TwoNums(int num1, int num2)
+            {
+                int Result = num1 + num2;
+
+
+                return Result;
+            }
+        }
+    }
+}
+      
